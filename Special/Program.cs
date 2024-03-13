11a4dfa4880e2195e6785ab939decff3a38f4184
a22007432 <ssm.antonio@gmail.com>
2024-03-13 18:17:22 +0000

    added ex 4

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,23 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(args[0]);
+            Console.WriteLine(GetSpecial(n));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            int ret = 0;
+            int[] result = new int[n];
+
+            if (n == 0){ret = 0;}
+            else if(n == 1){ret = 1;}
+            else
+            {
+                ret = GetSpecial(n -1) + (GetSpecial(n - 2)* 2);
+            }
+
+            return ret;
         }
     }
 }
